44b532bcdf0c6a02ff1c3e5b28afcb3a09844dd1
Olivia Brode-Roger <olivia.br@columntax.com>
2024-10-09 01:33:32 -0400

    Add indicators for where the selection currently is

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -7,7 +7,6 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Html.Keyed
-import Html.Lazy as Lazy
 import Json.Decode as Decode
 import List.Extra as List
 import Task
@@ -120,7 +119,13 @@ makeButton { isChecked, ix, row, col } =
         ]
 
 
-buttonViewer : { a | viewWidth : Int, viewHeight : Int, viewOffset : Int, nButtons : Int, checkedIx : Maybe Int } -> Html Msg
+type CheckedDirection
+    = Above
+    | Visible
+    | Below
+
+
+buttonViewer : Model -> Html Msg
 buttonViewer { viewWidth, nButtons, checkedIx, viewHeight, viewOffset } =
     let
         nPerRow : Int
@@ -143,54 +148,98 @@ buttonViewer { viewWidth, nButtons, checkedIx, viewHeight, viewOffset } =
             firstVisibleRow + nVisibleRows
 
         firstIx =
-            Basics.max 0 ((firstVisibleRow - 10) * nPerRow)
+            Basics.max 0 ((firstVisibleRow - 5) * nPerRow)
 
         lastIx =
-            (lastVisibleRow + 10) * nPerRow
-    in
-    Html.Keyed.node
-        "div"
-        [ Html.Attributes.style "height" ((nRows * rowHeight |> String.fromInt) ++ "px")
-        , Html.Attributes.style "width" ((nPerRow * buttonWidth |> String.fromInt) ++ "px")
-        , Html.Attributes.style "margin" "0"
-        , Html.Attributes.style "padding" "0"
-        , Html.Attributes.style "position" "relative"
-        , Html.Attributes.style "overflow" "scroll"
-        , Html.Events.on "scroll" (Decode.succeed GetNewViewport)
-        ]
-        (List.range firstIx lastIx
-            |> List.filter (\ix -> ix < nButtons)
-            -- for large lists, reverseMap >> reverse is much more memory efficient
-            |> List.reverseMap
-                (\ix ->
-                    let
-                        buttonData =
-                            { ix = ix
-                            , isChecked = checkedIx == Just ix
-                            , row = ix // nPerRow
-                            , col = modBy nPerRow ix
-                            }
-                    in
-                    (String.fromInt ix, makeButton buttonData)
+            Basics.min (nButtons - 1) ((lastVisibleRow + 5) * nPerRow)
+
+        checkboxes =
+            Html.Keyed.node
+                "div"
+                [ Html.Attributes.style "height" ((nRows * rowHeight |> String.fromInt) ++ "px")
+                , Html.Attributes.style "width" ((nPerRow * buttonWidth |> String.fromInt) ++ "px")
+                , Html.Attributes.style "margin" "0"
+                , Html.Attributes.style "padding" "0"
+                , Html.Attributes.style "position" "relative"
+                , Html.Attributes.style "overflow" "scroll"
+                , Html.Events.on "scroll" (Decode.succeed GetNewViewport)
+                ]
+                (List.range firstIx lastIx
+                    -- for large lists, reverseMap >> reverse is much more memory efficient
+                    |> List.reverseMap
+                        (\ix ->
+                            let
+                                buttonData =
+                                    { ix = ix
+                                    , isChecked = checkedIx == Just ix
+                                    , row = ix // nPerRow
+                                    , col = modBy nPerRow ix
+                                    }
+                            in
+                            -- (key, button)
+                            ( String.fromInt ix, makeButton buttonData )
+                        )
+                    |> List.reverse
                 )
-            |> List.reverse
-        )
+
+        checkedDirection =
+            checkedIx
+                |> Maybe.map
+                    (\cIx ->
+                        if cIx < firstIx then
+                            Above
+
+                        else if cIx > lastIx then
+                            Below
+
+                        else
+                            Visible
+                    )
+
+        arrow =
+            case checkedDirection of
+                Just Above ->
+                    Html.div
+                        [ Html.Attributes.style "position" "fixed"
+                        , Html.Attributes.style "top" "0px"
+                        , Html.Attributes.style "left" "0px"
+                        , Html.Attributes.style "text-align" "center"
+                        , Html.Attributes.style "width" "100%"
+                        , Html.Attributes.style "height" "4em"
+                        , Html.Attributes.style "background" "linear-gradient(180deg, #66339944, white)"
+                        ]
+                        []
+
+                Just Below ->
+                    Html.div
+                        [ Html.Attributes.style "position" "fixed"
+                        , Html.Attributes.style "bottom" "0px"
+                        , Html.Attributes.style "left" "0px"
+                        , Html.Attributes.style "text-align" "center"
+                        , Html.Attributes.style "width" "100%"
+                        , Html.Attributes.style "height" "4em"
+                        , Html.Attributes.style "background" "linear-gradient(0deg, #66339944, white)"
+                        ]
+                        []
+
+                _ ->
+                    Html.div [] []
+    in
+    Html.div
+        []
+        [ arrow, checkboxes ]
 
 
 view : Model -> Html Msg
-view model_ =
-    Lazy.lazy
-        (\model ->
-            let
-                buttons : Html Msg
-                buttons =
-                    buttonViewer model
-            in
-            Html.div
-                [ Html.Attributes.id "omrb-elm" ]
-                [ buttons ]
-        )
-        model_
+view model =
+    let
+        buttons : Html Msg
+        buttons =
+            buttonViewer model
+    in
+    Html.div
+        [ Html.Attributes.id "omrb-elm" ]
+        [ buttons ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
